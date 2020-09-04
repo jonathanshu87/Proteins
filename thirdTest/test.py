9bd6868d50ad296b4172179bf32d3042a9dd6ae7
Jonathan Shu <jonathanshu@jonathans-macbook-pro.local>
2020-09-04 11:23:31 -0700

    commit

--- thirdTest/test.py
+++ thirdTest/test.py
@@ -34,27 +34,27 @@ with open('nP20k.fasta', 'rU') as handle:
             recordstr = recordstr.replace('U', 'L')
         if record.description.__contains__('PE=2'):
             analyzed_seq = ProteinAnalysis(recordstr)
-            tup = (record.id, '1', analyzed_seq.gravy(), textwrap.fill(record.description, 20))
+            tup = (record.id, '1', analyzed_seq.gravy(), record.description)
             info.append(tup)
         if record.description.__contains__('PE=3'):
             analyzed_seq = ProteinAnalysis(recordstr)
-            tup = (record.id, '3', analyzed_seq.gravy(), textwrap.fill(record.description, 20))
+            tup = (record.id, '3', analyzed_seq.gravy(), record.description)
             info.append(tup)
         if record.description.__contains__('PE=4'):
             analyzed_seq = ProteinAnalysis(recordstr)
-            tup = (record.id, '4', analyzed_seq.gravy(), textwrap.fill(record.description, 20))
+            tup = (record.id, '4', analyzed_seq.gravy(), record.description)
             info.append(tup)
 
-column_name_list = [ 'Identifier', 'PE', 'GRAVY', 'Description' ]
+column_name_list = [ 'Identifier', 'PE', '    GRAVY', '            Description' ]
 proteins = pd.DataFrame(info, columns = column_name_list)
-proteins.to_csv('PE1_proteins.tsv',sep='\t')
+proteins.to_csv('PE 234_proteins.tsv',sep='\t')
 
 
 min = -2
 max = 2
 binsize = 0.1
 n_bins = int((max-min) / binsize)
-count, x_floor, patches = plt.hist(proteins['GRAVY'], n_bins, [min,max], density = False, facecolor = 'r', alpha = 0.5)
+count, x_floor, patches = plt.hist(proteins['    GRAVY'], n_bins, [min,max], density = False, facecolor = 'r', alpha = 0.5)
 plt.title('Distribution of hydrophobicity of PE 2,3,4 proteins')
 plt.xlabel('Hydrophobicity (GRAVY score)')
 plt.ylabel('Proteins')
