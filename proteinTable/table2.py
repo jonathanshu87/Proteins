c0ba33d6a7b95434e1e87fbb7cd9e2fc52ba80b4
Jonathan Shu <jonathan.shu2@gmail.com>
2021-03-04 14:33:54 -0800

    automatic download of missing files

--- proteinTable/table2.py
+++ proteinTable/table2.py
@@ -222,69 +222,69 @@ for identifier in proteins:
 # Test with just one chromosome, then do the final run with the whole proteome
 # Input files can be either compressed on uncompressed
 #input_file = 'G:/Data/nextprot/nextprot_release_2020-01-17/xml/nextprot_chromosome_21.xml.gz'
-input_file = 'nextprot_all.xml.gz'
 
+input_file = 'nextprot_all.xml.gz'
+n_tmr = {}
 if os.path.isfile('nextprot_all.xml.gz')==False:
     print ('INFO: Downloading nextprot_all.xml.gz')
     link3 = 'ftp://ftp.nextprot.org/pub/current_release/xml/nextprot_all.xml.gz'
     wget.download(link3)
-    
+
+print('INFO: Reading nextprot_all.xml.gz')
 # If the file ends with gz, then open it as a gzipped stream
-    is_compressed = False
-    if input_file.endswith('.gz'):
-        infile = gzip.open(input_file)
-        is_compressed = True
-    else:
-        infile = open(input_file, 'r', encoding="utf-8", errors="replace")
+is_compressed = False
+if input_file.endswith('.gz'):
+    infile = gzip.open(input_file)
+    is_compressed = True
+else:
+    infile = open(input_file, 'r', encoding="utf-8", errors="replace")
 
-    buffer = ''
-    state = 'outside_entry'
+buffer = ''
+state = 'outside_entry'
 
-    for line in infile:
-
+for line in infile:
     # If we're reading a compressed stream, each line is actually a byte array, so convert to a string
-        if is_compressed:
-            line = line.decode('utf-8')
+    if is_compressed:
+        line = line.decode('utf-8')
 
     # If we're not inside an entry and we see the start of a entry, begin buffering
-        if state == 'outside_entry' and '<entry accession' in line:
-            state = 'in_entry'
+    if state == 'outside_entry' and '<entry accession' in line:
+        state = 'in_entry'
 
     # If we're inside an entry and find the end tag, end the buffering
-        elif state == 'in_entry' and '</entry>' in line:
-            state = 'parse_entry'
-            buffer += line
+    elif state == 'in_entry' and '</entry>' in line:
+        state = 'parse_entry'
+        buffer += line
 
     # If inside an entry, add this line to the buffer
-        if state == 'in_entry':
-            buffer += line
+    if state == 'in_entry':
+        buffer += line
 
     # If we just finished an entry, then parse the XML in the buffer and extract what we need
-        if state == 'parse_entry':
-            doc = etree.fromstring(buffer)
-            for entry in doc.xpath('//entry'):
-                counter = 0
-                match = re.match(r'NX_(.*)', entry.attrib['accession'])
-                identifier = match.group(1)
+    if state == 'parse_entry':
+        doc = etree.fromstring(buffer)
+        for entry in doc.xpath('//entry'):
+            counter = 0
+            match = re.match(r'NX_(.*)', entry.attrib['accession'])
+            identifier = match.group(1)
 
             # Loop over the CV terms
-                cvterms = entry.xpath('.//cv-term[@terminology="nextprot-topology-cv"]/text()')
-                for item in cvterms:
-                    if item == "Transmembrane region":
-                        counter += 1
-                    
-                proteins[identifier] = {}
-                proteins[identifier]['Identifier'] = identifier
-                proteins[identifier]['n_TMs'] = counter
-                print(f"{identifier}\t{counter}")
+            cvterms = entry.xpath('.//cv-term[@terminology="nextprot-topology-cv"]/text()')
+            for item in cvterms:
+                if item == "Transmembrane region":
+                    counter += 1
+            n_tmr[identifier] = {}
+            n_tmr[identifier]['Identifier'] = identifier
+            n_tmr[identifier]['n_TMs'] = counter
+            # print(f"{identifier}\t{counter}")
 
         # Done parsing this entry, reset the buffer
-            state = 'outside_entry'
-            buffer = ''
+        state = 'outside_entry'
+        buffer = ''
 
 # Convert the dict to a dataframe and write as xlsx
 if os.path.isfile('tmr_table.xlsx')==False:
-    df = pd.DataFrame(proteins)
+    df = pd.DataFrame(n_tmr)
     df_t = df.transpose()
     df_t = df_t[['Identifier', 'n_TMs']]
     df_t.columns = ['Identifier', 'n_TMRs']
@@ -358,7 +358,6 @@ gene = []
 for identifier in proteins:
     ident.append(proteins[identifier]['Identifier'])
     gene.append(proteins[identifier]['Symbol'])
-print('length of gene list: ', len(gene))
 for x in uniprot1:
     if x not in ident:
         counter += 1
